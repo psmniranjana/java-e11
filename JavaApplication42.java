c70fe5a856bd97b71f0c60f47265f2f0e8701bb0
Maleesha Niranjana <117069472+psmniranjana@users.noreply.github.com>
2023-04-26 12:35:45 +0530

    Add files via upload

---
+++ JavaApplication42.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication42;
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class JavaApplication42 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+
+
+       Scanner read = new Scanner(System.in);
+       char a = read.next().charAt(1);
+       int b=(int) a;
+       System.out.println(b);
+       //your code goes here
+       
+    }   
+}
+    
